0f07ff2e8985ce653e8c02c67bf6f2e3b1f9dda4
ndeskaj <n.deskaj@gmx.de>
2024-06-28 01:37:35 +0200

    second plot for SP; includes collapsible boxes and introduction to topic

--- prototype/app.R
+++ prototype/app.R
@@ -14,11 +14,11 @@ source("R/app4_module.R")
 
 # main UI
 ui <- dashboardPage(
-  dashboardHeader(title = "Shiny Apps Dashboard"),
+  dashboardHeader(title = "Statistical Illusions"),
   dashboardSidebar(
     sidebarMenu(
       menuItem("Home", tabName = "home", icon = icon("home")),
-      menuItem("App 1", tabName = "app1", icon = icon("bar-chart")),
+      menuItem("Simpson's Paradox", tabName = "app1", icon = icon("bar-chart")),
       menuItem("App 2", tabName = "app2", icon = icon("line-chart")),
       menuItem("App 3", tabName = "app3", icon = icon("pie-chart")),
       menuItem("St. Petersburg Paradox", tabName = "app4", 
